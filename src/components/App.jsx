dc3cd7cef8985caeda912d3a74ec3c1b5749eaae
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-05-23 00:59:47 +0300

    tmp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import axios from 'axios';
+import Select from 'react-select';
+// import { GlobalStyle } from './GlobalStyle';
+
+axios.defaults.baseURL = 'https://api.thedogapi.com/v1';
+axios.defaults.headers.common['x-api-key'] = process.env.REACT_APP_API_KEY;
+
+// ##################################################
+
+export class App extends Component {
+  state = {
+    breeds: [],
+    dog: null,
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get('/breeds');
+      this.setState({ breeds: response.data });
+    } catch (error) {}
+  }
+
+  selectBreed = async option => {
+    try {
+      const response = await axios.get('/images/search', {
+        params: {
+          breed_ids: option.value,
+        },
+      });
+
+      this.setState({ dog: response.data[0] });
+    } catch (error) {}
+  };
+
+  buildSelectOptions = () => {
+    return this.state.breeds.map(breed => ({
+      value: breed.id,
+      label: breed.name,
+    }));
+  };
+
+  render() {
+    const options = this.buildSelectOptions();
+    const { dog } = this.state;
+
+    return (
+      <>
+        <Select options={options} onChange={this.selectBreed} />
+
+        {dog && (
+          <div>
+            <img src={dog.url} width="480" alt="dog" />
+          </div>
+        )}
+
+        {/* <GlobalStyle/> */}
+      </>
+    );
+  }
+}
